3047898d1fdd628441b60ad180bbfc0dfbe4dee1
noprysk <noprysk@n-ix.com>
2015-04-29 17:39:41 +0300

    Java 8 new features

---
+++ src/main/java/nao/cycledev/java8test/functionalinterfaces/Converter.java
@@ -0,0 +1,7 @@
+package nao.cycledev.java8test.functionalinterfaces;
+
+/**
+ * Created by noprysk on 29.04.2015.
+ */
+public interface Converter {
+}
---
+++ src/main/java/nao/cycledev/java8test/streams/StreamTest.java
@@ -0,0 +1,7 @@
+package nao.cycledev.java8test.streams;
+
+/**
+ * Created by noprysk on 29.04.2015.
+ */
+public class StreamTest {
+}
